49118de30120778f9da90577b55c49f1b3fbd086
douglas medeiros <douglasjfm@gmail.com>
2023-06-08 21:33:40 -0300

    remocao 2 threads

--- main.cpp
+++ main.cpp
@@ -11,46 +11,6 @@ void print_output(int x, BI& a, clock_t tm)
     std::cout << "output length: " << a.toStr().size() << " digits" << std::endl;
 }
 
-void print_fac2(int x)
-{
-    int i = x-1, j = 1, x2 = x>>1;
-    BI a = x, b = 1;
-    clock_t tm = clock();
-
-    if (x < 1)
-    {
-        return;
-    }
-
-    std::thread ta([&a,&i,&j](){
-        while(i > j)
-        {
-            a = a * i;
-            i--;
-        }
-        std::cout << "FIM A " << i << std::endl;
-    });
-    std::thread tb([&b,&j,&i](){
-        while(j < i)
-        {
-            b = b * j;
-            j++;
-        }
-        std::cout << "FIM B " << j << std::endl;
-    });
-
-    if (ta.joinable()) ta.join();
-    if (tb.joinable()) tb.join();
-
-    std::cout << "FIM C " << std::endl;
-    
-    a = a * b;
-
-    std::cout << "FIM D" << std::endl;
-
-    print_output(x,a,tm);
-}
-
 void print_fac(int x)
 {
     int i = x-1;
@@ -83,8 +43,7 @@ int main(int argc, char **argv)
         fat = 10000;
     }
 
-    if (argc == 3 || argc == 1) print_fac2(fat);
-    else print_fac(fat);
+    print_fac(fat);
 
     return 0;
 }
